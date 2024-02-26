84bc9b2456d56da774a6c2535905ebb063842ea7
Tashreeqlee <tashyct14@gmail.com>
2024-02-26 12:10:22 +0200

    static files added and changes to controller

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -5,7 +5,7 @@ import { products } from "../model/index.js";
 const productRouter = express.Router();
 
 //fetch all products
-productRouter.get("/", (req, res) => {
+productRouter.get("/products", (req, res) => {
   try {
     products.fetchProducts(req, res);
   } catch (e) {
@@ -32,6 +32,28 @@ productRouter.post("/addProduct", bodyParser.json(), (req, res) => {
     res.json({
       status: res.statusCode,
       msg: "Failed to add a product",
+    });
+  }
+});
+
+productRouter.delete("/product/:id", (req, res)=>{
+  try{
+      users.deleteProduct(req, res)
+  }catch(e) {
+      res.json({
+          status: res.statusCode,
+          msg: "Unable to change a product",
+        });
+  }
+});
+
+productRouter.patch("/product/:id", bodyParser.json(), (req, res) => {
+  try {
+    users.updateProduct(req, res);
+  } catch (e) {
+    res.json({
+      status: res.statusCode,
+      msg: "Failed to update a product",
     });
   }
 });
