280556d4e3b7171b4d674fa17d8ed60cc5a0fefc
Tashreeqlee <tashyct14@gmail.com>
2024-02-29 14:37:56 +0200

    ProductsView working

---
+++ src/components/CardComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <router-link :to="{ name: 'product', params: { id: product.prodID } }">
+      <div class="card">
+        <img
+          :src="product.prodUrl"
+          class="card-img-top"
+          :alt="product.prodName"
+        />
+        <div class="card-body">
+          <h5 class="card-title">{{ product.prodName }}</h5>
+          <p class="card-text">Price: R {{ product.amount }}</p>
+        </div>
+      </div>
+    </router-link>
+  </template>
+
+
+
+<script>
+    export default {
+  props: ["product"],
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,16 +1,99 @@
 <template>
-    <div>
-        <h1>Products</h1>
-
+    <h1 class="text-center">
+      Products
+    </h1>
+    <div class="buttons">
+      <div class="search">
+        <input type="text" v-model="search" placeholder="search" />
+      </div>
+      <div>
+        <button @click="sortByPrice" class="sort">Sort by Price</button>
+        <button @click="sortByName" class="sort">Sort by Name</button>
+      </div>
     </div>
-</template>
+  
+    <div v-if="Products" class="display container-fluid">
+      <CardComp
+        v-for="product of Products"
+        :key="product.prodID"
+        :product="product"
+      />
+    </div>
+    <div v-else class="d-flex justify-content-center">
+      <SpinnerComp/>
+    </div>
+  </template>
 
 <script>
-    export default {
-        
-    }
+import CardComp from '@/components/CardComp.vue';
+import SpinnerComp from '@/components/SpinnerComp.vue';
+
+export default {
+  data() {
+    return {
+      search: "",
+      Categories: "All",
+    };
+  },
+  methods: {
+    sortByPrice() {
+      this.$store.commit("sortProducts");
+    },
+    sortByName() {
+      this.$store.commit("sortByName");
+    },
+  },
+  computed: {
+    Products() {
+      return this.$store.state.products?.filter((product) => {
+        let isMatch = true;
+        if (
+          !product.prodName.toLowerCase().includes(this.search.toLowerCase())
+        ) {
+          isMatch = false;
+        }
+        if (
+          this.Categories !== "All" &&
+          this.Categories !== product.Categories
+        ) {
+          isMatch = false;
+        }
+        return isMatch;
+      });
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchProducts");
+  },
+  components: {
+    CardComp,
+    SpinnerComp,
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style>
+
+
+.buttons {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+}
+
+.search {
+  flex: 10;
+}
+
+.sort {
+  background-color: #EF370E;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+  margin-right: 10px;
+}
+
+
 
 </style>
