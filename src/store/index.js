8edfb0bff6a4ab3b923e090490d9ad6e93757466
Tashreeqlee <tashyct14@gmail.com>
2024-02-29 11:47:06 +0200

    added store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,282 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import sweet from 'sweetalert'
+import { useCookies } from 'vue3-cookies'
+const {cookies} = useCookies()
+import router from '@/router'
+import AuthenticateUser from '@/service/Authenticateuser.js'
+
+const matrixUrl = 'https://nodejseomp-fpm6.onrender.com/';
 
 export default createStore({
   state: {
+    user: null,
+    users: null,
+    product: null,
+    products: null,
+    asc: true,
   },
   getters: {
   },
   mutations: {
+    setProducts: (state, value) => {
+      state.products = value
+    },
+    setProduct: (state, value) => {
+      state.product= value
+    },
+    sortProducts: (state) => {
+      state.products.sort ((a, b) => {
+        return a.Amount - b.Amount;
+      });
+      if (!state.asc) {
+        state.products.reverse();
+      }
+      state.asc= !state.asc
+    },
+    sortByName: (state) => {
+      state.products.sort((a, b) => {
+        if (a.prodName < b.prodName) {
+          return -1;
+        }
+        if (a.prodName > b.prodName) {
+          return 1;
+        }
+        return 0;
+      });
+      if (!state.asc) {
+        state.products.reverse();
+      }
+      state.asc = !state.asc
+    },
+   
+    setUsers: (state, value) => {
+      state.users= value
+    }
   },
   actions: {
+    //products
+    async fetchProducts(context) {
+      try{
+        let {results} = (await axios.get(`${matrixUrl}products`)).data
+        if(results) {
+          context.commit('setProducts', results)
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'An error occurred when retrieving products.',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+    async fetchProduct(context, payload) {
+      try{
+        let {result} = (await axios.get(`${matrixUrl}products/${payload.id}`)).data
+        if(result) {
+          context.commit('setProduct', result)
+        }else {
+          sweet({
+            title: 'Retrieving a single product',
+            text: 'Product was not found',
+            icon: "info",
+            timer: 2000
+          }) 
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'A product was not found.',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+    async updateProduct(context, payload) {
+      try{
+        let {msg} = await axios.patch(`${matrixUrl}products/update/${payload.id}`)
+        if(msg) {
+          context.dispatch('fetchProducts')
+          sweet({
+            title: 'Update product',
+            text: msg,
+            icon: "success",
+            timer: 2000
+          }) 
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'An error occurred when updating a product.',
+          icon: "success",
+          timer: 2000
+        })
+      }
+    },
+    async deleteProduct(context, payload) {
+      try{
+        let {msg} = await axios.delete(`${matrixUrl}products/${payload.id}`)
+        if(msg) {
+          context.dispatch('fetchProducts')
+          sweet({
+            title: 'Delete product',
+            text: msg,
+            icon: "success",
+            timer: 2000
+          }) 
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'An error occurred when deleting a product.',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+
+    //users
+    async register(context, payload) {
+      try{
+        let {msg} = (await axios.post(`${matrixUrl}users/register`, payload)).data
+        if(msg) {
+          context.dispatch('fetchUsers')
+          sweet({
+            title: 'Registration',
+            text: msg,
+            icon: "success",
+            timer: 2000
+          }) 
+          //  
+          router.push({name: 'login'})
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'Please try again later',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+    async fetchUsers(context) {
+      try{
+        let {results} = (await axios.get(`${matrixUrl}users`)).data
+        if(results) {
+          context.commit('setUsers', results)
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'An error occurred when retrieving users.',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+    async fetchUser(context, payload) {
+      try{
+        let {result} = (await axios.get(`${matrixUrl}users/${payload.id}`)).data
+        if(result) {
+          context.commit('setUser', result)
+        }else {
+          sweet({
+            title: 'Retrieving a single user',
+            text: 'User was not found',
+            icon: "info",
+            timer: 2000
+          }) 
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'A user was not found.',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+    async updateUser(context, payload) {
+      try{
+        let {msg} = await axios.patch(`${matrixUrl}users/update/${payload.id}`)
+        if(msg) {
+          context.dispatch('fetchUsers')
+          sweet({
+            title: 'Update user',
+            text: msg,
+            icon: "success",
+            timer: 2000
+          }) 
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'An error occurred when updating a user.',
+          icon: "success",
+          timer: 2000
+        }) 
+      }
+    },
+    async deleteUser(context, payload) {
+      try{
+        let {msg} = await axios.delete(`${matrixUrl}users/${payload.id}`)
+        if(msg) {
+          context.dispatch('fetchUsers')
+          sweet({
+            title: 'Delete user',
+            text: msg,
+            icon: "success",
+            timer: 2000
+          }) 
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'An error occurred when deleting a user.',
+          icon: "error",
+          timer: 2000
+        }) 
+      }
+    },
+    async login(context, payload) {
+      try{
+       const {msg, token, result} = (await axios.post(`${matrixUrl}users/login`, payload)).data 
+       if(result){
+        context.commit('setUser', {msg, result})
+        cookies.set('LegitUser', {
+          msg, token, result
+        })
+        AuthenticateUser.applyToken(token)
+        sweet({
+          title: msg,
+          text: `Welcome back, 
+          ${result?.firstName} ${result?.lastName}`,
+          icon: "success",
+          timer: 2000
+        })
+          router.push({name: 'home'})
+        }else {
+          sweet({
+            title: 'info',
+            text: msg,
+            icon: "info",
+            timer: 2000
+          })
+        }
+      }catch(e) {
+        sweet({
+          title: 'Error',
+          text: 'Failed to login.',
+          icon: "error",
+          timer: 2000
+        })
+      }
+      
+
+    },
   },
   modules: {
   }
 })
+
